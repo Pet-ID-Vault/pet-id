2680a124324b465b8faf941a03e795bd602a1214
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2024-07-14 06:02:04 +0200

    feat: verify contract on blockscout
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -7,6 +7,7 @@ import "fhenix-hardhat-docker";
 import "fhenix-hardhat-plugin";
 import "hardhat-deploy";
 import { HardhatUserConfig } from "hardhat/config";
+import "@xyrusworx/hardhat-solidity-json";
 
 const TESTNET_CHAIN_ID = 8008135;
 const TESTNET_RPC_URL = "https://api.helium.fhenix.zone";
@@ -40,6 +41,24 @@ const config: HardhatUserConfig = {
   defaultNetwork: "localfhenix",
   networks: {
     testnet: testnetConfig,
+  },
+  etherscan: {
+    apiKey: {
+      testnet: "0xabc"
+    },
+    customChains: [
+      {
+        network: "testnet",
+        chainId: TESTNET_CHAIN_ID,
+        urls: {
+          apiURL: "https://explorer.helium.fhenix.zone/api",
+          browserURL: "https://explorer.helium.fhenix.zone/",
+        }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: false,
   },
   typechain: {
     outDir: "types",
